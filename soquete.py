c9ecb23f81d3c004533f9f8cb40451722d2b31c2
Rafa3141 <rei42782@gmail.com>
2023-08-25 13:11:04 -0300

    two files

---
+++ soquete.py
@@ -0,0 +1,7 @@
+import socket 
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("bancocn.com", 80))
+client.send(b"hello world")
+resposta = client.recv(1024)
+print(resposta.decode())
